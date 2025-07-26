ac538bfaed3e3d7292d69d6c9bc03d4a9c63816b
BeastRWA <rushikeshwaghmode688@gmail.com>
2025-07-26 18:39:47 +0530

    Configure Vite for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/rushikesh_rw/", // Add this line!
   plugins: [react()],
   resolve: {
     alias: {
