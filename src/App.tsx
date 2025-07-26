94183f9ebd10fd8b11a9551844bfb6b4cebbe292
BeastRWA <rushikeshwaghmode688@gmail.com>
2025-07-26 19:28:44 +0530

    Fix router base path

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router as WouterRouter } from "wouter"; // Import Router as WouterRouter
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -9,10 +9,13 @@ import NotFound from "@/pages/not-found";
 
 function Router() {
   return (
-    <Switch>
-      <Route path="/" component={Portfolio} />
-      <Route component={NotFound} />
-    </Switch>
+    // Add the Router wrapper with the correct base path
+    <WouterRouter base="/rushikesh_rw">
+      <Switch>
+        <Route path="/" component={Portfolio} />
+        <Route component={NotFound} />
+      </Switch>
+    </WouterRouter>
   );
 }
 
